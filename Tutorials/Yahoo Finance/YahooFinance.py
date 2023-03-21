073f7c2867ce5adbf69a30029493e902d059d3d9
hou71 <ahou9@gatech.edu>
2023-03-20 22:01:01 -0400

    testing APIs

---
+++ Tutorials/Yahoo Finance/YahooFinance.py
@@ -0,0 +1,10 @@
+import yfinance as yf
+
+# https://algotrading101.com/learn/yahoo-finance-api-guide/
+# https://pypi.org/project/yfinance/
+
+def get_data():
+    pass
+
+if __name__ == "__main__":
+    get_data()
